9a72e6c98903ca42672b38a85d35303ece452c3d
Jeno Sam S. Flores <jenosam011703@gmail.com>
2025-04-12 21:45:20 +0800

    All origin

--- backend.py
+++ backend.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Allow all origins (for CORS)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://testing-git-main-sunderingbeavers-projects.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
